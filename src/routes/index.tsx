9d101dd6a383d248e7ed0748c665bc7c423db9a5
Sohrab Chegini <sohrab.chegini@gmail.com>
2025-01-04 16:50:29 +0330

    fix index route export

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-export const IndexRoute = createFileRoute("/")({
+const IndexRoute = createFileRoute("/")({
   component: () => {
     return <></>;
   },
